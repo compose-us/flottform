2d956a1c1cfcd35bec820f1778dfaededfd387d3
Joern Bernhardt <joern.bernhardt@googlemail.com>
2024-03-04 19:20:58 +0100

    chore: Use process.env instead of sveltekit
    Signed-off-by: Joern Bernhardt <joern.bernhardt@googlemail.com>

--- flottform/demo/vite.config.ts
+++ flottform/demo/vite.config.ts
@@ -1,9 +1,8 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
 import basicSsl from '@vitejs/plugin-basic-ssl';
-import { env } from '$env/dynamic/public';
 
-const isDemoEnvironment = !!env.PUBLIC_IS_ONLINE_DEMO;
+const isDemoEnvironment = process.env.PUBLIC_IS_ONLINE_DEMO;
 
 export default defineConfig({
 	plugins: [sveltekit(), ...(isDemoEnvironment ? [basicSsl()] : [])],
