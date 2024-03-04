3c36d0e549249d0bed87faf166f0be1d74ad5b57
Joern Bernhardt <joern.bernhardt@googlemail.com>
2024-03-04 16:21:46 +0100

    chore: Setup for docker
    Signed-off-by: Joern Bernhardt <joern.bernhardt@googlemail.com>

---
+++ Dockerfile
@@ -0,0 +1,9 @@
+ARG NODE_VERSION
+
+FROM node:${NODE_VERSION} AS runner
+WORKDIR /app
+COPY package.json pnpm-lock.yaml* pnpm-workspace.yaml ./
+RUN corepack enable && corepack prepare
+RUN pnpm install
+
+CMD [ "pnpm", "-r", "--if-present", "--parallel", "run", "dev" ]
--- flottform/demo/src/api.ts
+++ flottform/demo/src/api.ts
@@ -1,5 +1,12 @@
-export const sdpExchangeServerBase = "https://192.168.0.21:5177/flottform"
+import { browser } from '$app/environment';
+import { env } from '$env/dynamic/public';
 
-export const createClientUrl= async ({ endpointId }:{ endpointId:string }) => {
-    return `https://192.168.0.21:5173/flottform-client/${endpointId}`;
-}
+export const sdpExchangeServerBase =
+	env.PUBLIC_FLOTTFORM_SERVER_BASE || 'https://192.168.0.21:5177/flottform';
+
+export const createClientUrl = async ({ endpointId }: { endpointId: string }) => {
+	if (browser) {
+		return `${window.location.origin}/flottform-client/${endpointId}`;
+	}
+	return `https://192.168.0.21:5173/flottform-client/${endpointId}`;
+};
