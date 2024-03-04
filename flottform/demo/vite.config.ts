e1272cae0bcac290ef18b8c90e9c936fc621a4c4
Joern Bernhardt <joern.bernhardt@googlemail.com>
2024-03-04 19:24:55 +0100

    chore: Invert boolean logic
    Signed-off-by: Joern Bernhardt <joern.bernhardt@googlemail.com>

--- flottform/demo/vite.config.ts
+++ flottform/demo/vite.config.ts
@@ -5,10 +5,10 @@ import basicSsl from '@vitejs/plugin-basic-ssl';
 const isDemoEnvironment = process.env.PUBLIC_IS_ONLINE_DEMO;
 
 export default defineConfig({
-	plugins: [sveltekit(), ...(isDemoEnvironment ? [basicSsl()] : [])],
+	plugins: [sveltekit(), ...(isDemoEnvironment ? [] : [basicSsl()])],
 	server: {
 		cors: true,
-		...(isDemoEnvironment ? { proxy: {} } : {})
+		...(isDemoEnvironment ? {} : { proxy: {} })
 	},
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
