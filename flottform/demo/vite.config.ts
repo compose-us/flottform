24d7c212709fe36fe864068b66003f7c35f66ae2
Joern Bernhardt <joern.bernhardt@googlemail.com>
2024-03-04 19:16:58 +0100

    chore: Do not use SSL in online demo internally
    Signed-off-by: Joern Bernhardt <joern.bernhardt@googlemail.com>

--- flottform/demo/vite.config.ts
+++ flottform/demo/vite.config.ts
@@ -1,10 +1,15 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
+import basicSsl from '@vitejs/plugin-basic-ssl';
+import { env } from '$env/dynamic/public';
+
+const isDemoEnvironment = !!env.PUBLIC_IS_ONLINE_DEMO;
 
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [sveltekit(), ...(isDemoEnvironment ? [basicSsl()] : [])],
 	server: {
-		cors: true
+		cors: true,
+		...(isDemoEnvironment ? { proxy: {} } : {})
 	},
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
