2cdd4841754950a0a2f5cb0a300707039ae9c965
Joern Bernhardt <joern.bernhardt@googlemail.com>
2024-03-06 13:16:38 +0100

    chore: Use one stun server only after firefox complained
    Signed-off-by: Joern Bernhardt <joern.bernhardt@googlemail.com>

--- flottform/forms/src/internal.ts
+++ flottform/forms/src/internal.ts
@@ -21,13 +21,7 @@ export type ClientState = 'loading' | 'waiting-for-file' | 'sending-file' | 'don
 export const DEFAULT_WEBRTC_CONFIG: RTCConfiguration = {
 	iceServers: [
 		{
-			urls: [
-				'stun:stun.l.google.com:19302',
-				'stun:stun1.l.google.com:19302',
-				'stun:stun2.l.google.com:19302',
-				'stun:stun3.l.google.com:19302',
-				'stun:stun4.l.google.com:19302'
-			]
+			urls: ['stun:stun.l.google.com:19302']
 		}
 	]
 };
