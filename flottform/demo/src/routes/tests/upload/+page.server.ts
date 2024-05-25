8691995d4bd47fe29a9a94d664ecf9456f1a8a2f
TamaraBogantseva <tamara.bogantseva@compose.us>
2024-05-25 10:15:46 +0200

    chore: update tests and create an upload endpoint for test
    Signed-off-by: TamaraBogantseva <tamara.bogantseva@compose.us>

--- flottform/demo/src/routes/tests/upload/+page.server.ts
+++ flottform/demo/src/routes/tests/upload/+page.server.ts
@@ -1,36 +1,17 @@
 import { fail } from '@sveltejs/kit';
 import type { Actions } from './$types';
-import { Writable } from 'node:stream';
-import { createWriteStream } from 'node:fs';
-import { mkdir, readdir, stat, unlink } from 'node:fs/promises';
-import { env } from '$env/dynamic/private';
-
-const UPLOAD_FOLDER = env.UPLOAD_FOLDER ?? 'static/uploads/tests';
 
 export const actions: Actions = {
 	default: async ({ request }) => {
 		console.log('got a request');
 		const fd = await request.formData();
 		const document = fd.get('document');
-		if (!isFile(document)) {
-			return fail(422, { error: true, message: 'document needs to be a file' });
+		if (!isFile(document) || !isString(document)) {
+			return fail(422, { error: true, message: 'document needs to be a file or a string' });
 		}
 
-		console.log('creating folder if does not exist', UPLOAD_FOLDER);
-		await mkdir(UPLOAD_FOLDER, { recursive: true });
-
-		const uuid = crypto.randomUUID();
-		const filenameDoc = `${uuid}-${document.name}`;
-		const filePath = `${UPLOAD_FOLDER}/${filenameDoc}`;
-		console.log('creating/uploading', filePath);
-		const writeStream = Writable.toWeb(createWriteStream(filePath));
-
-		await Promise.allSettled([document.stream().pipeTo(writeStream), tryRemovingOldUploads()]);
-
 		return {
-			success: true,
-			document: uuid,
-			filenameDoc
+			success: true
 		};
 	}
 };
@@ -41,19 +22,6 @@ function isFile(x: any): x is File {
 	);
 }
 
-async function tryRemovingOldUploads(): Promise<void> {
-	try {
-		const uploadFolderList = await readdir(UPLOAD_FOLDER);
-		for (const file of uploadFolderList) {
-			const fileName = `${UPLOAD_FOLDER}/${file}`;
-			const fileStats = await stat(fileName);
-			const lastEdited = fileStats.mtimeMs || fileStats.atimeMs || fileStats.ctimeMs;
-			const twoHoursAgo = +new Date() - 1000 * 60 * 60 * 2;
-			if (lastEdited < twoHoursAgo) {
-				await unlink(fileName);
-			}
-		}
-	} catch (err) {
-		console.log('Could not delete old uploads:', err);
-	}
+function isString(x: any): x is string {
+	return typeof x === 'string';
 }
