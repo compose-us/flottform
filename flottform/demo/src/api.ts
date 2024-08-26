9007a5bd7f9a712cbcd0450f40de9dc52873e820
nidhal-labidi <nidhal.labidi@compose.us>
2024-08-26 11:10:20 +0200

    fix: make the original demo use the correct client
    Signed-off-by: nidhal-labidi <nidhal.labidi@compose.us>

--- flottform/demo/src/api.ts
+++ flottform/demo/src/api.ts
@@ -5,12 +5,11 @@ import { env } from '$env/dynamic/public';
 export const sdpExchangeServerBase =
 	env.PUBLIC_FLOTTFORM_SERVER_BASE || 'https://192.168.0.21:5177/flottform';
 
-const clientBase =
-	env.PUBLIC_FLOTTFORM_CLIENT_BASE || 'https://192.168.0.21:5173/multiple-input-form-client';
+const clientBase = env.PUBLIC_FLOTTFORM_CLIENT_BASE || 'https://192.168.0.21:5173/flottform-client';
 
 export const createClientUrl = async ({ endpointId }: { endpointId: string }) => {
 	if (browser) {
-		return `${window.location.origin}${base}/multiple-input-form-client/${endpointId}`;
+		return `${window.location.origin}${base}/flottform-client/${endpointId}`;
 	}
 	return `${clientBase}/${endpointId}`;
 };
