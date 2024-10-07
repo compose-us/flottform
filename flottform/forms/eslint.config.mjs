fb2889afd62b598a19fdcae9f6861c98f24676d9
Joern Bernhardt <joern.bernhardt@googlemail.com>
2024-10-07 11:25:16 +0200

    chore: Add ignore pattern for build/dist artifacts
    Signed-off-by: Joern Bernhardt <joern.bernhardt@googlemail.com>

--- flottform/forms/eslint.config.mjs
+++ flottform/forms/eslint.config.mjs
@@ -15,6 +15,7 @@ const compat = new FlatCompat({
 });
 
 export default [
+	{ ignores: ['**/node_modules', 'dist'] },
 	...compat.extends('eslint:recommended', 'plugin:@typescript-eslint/recommended'),
 	{
 		plugins: {
