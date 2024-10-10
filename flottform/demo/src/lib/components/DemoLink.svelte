7929c272a63c4fbe59a7b4ec30ecc3486821c0c6
TamaraBogantseva <tamara.bogantseva@compose.us>
2024-10-10 11:53:38 +0200

    chore: make homepage look more like flottform
    Signed-off-by: TamaraBogantseva <tamara.bogantseva@compose.us>

--- flottform/demo/src/lib/components/DemoLink.svelte
+++ flottform/demo/src/lib/components/DemoLink.svelte
@@ -15,12 +15,21 @@
 	} & HTMLAttributes<HTMLLinkElement | HTMLAnchorElement> = $props();
 </script>
 
-<a
-	{href}
-	class="p-6 rounded bg-slate-200 flex flex-col gap-2 md:max-w-80 hover:shadow-lg hover:scale-105 transform duration-150 {linkClass}"
-	{...rest}
-	><h2 class="text-2xl text-center">{title}</h2>
-	<p class="italic">
-		{description}
-	</p>
+<a {href} class="contents {linkClass}" {...rest}>
+	<div class="perspective">
+		<h2 class="text-2xl">{title}</h2>
+		<p class="italic text-lg">
+			{description}
+		</p>
+	</div>
 </a>
+
+<style lang="postcss">
+	.perspective {
+		@apply relative text-black p-8 grid gap-8 rounded-lg bg-white m-[2px];
+	}
+	.perspective::before {
+		@apply absolute rounded-lg shadow-lg hover:shadow-2xl bg-gradient-to-br from-secondary-blue to-secondary-green -inset-[2px] -z-10;
+		content: '';
+	}
+</style>
--- flottform/demo/src/routes/+page.svelte
+++ flottform/demo/src/routes/+page.svelte
@@ -1,15 +1,44 @@
 <script lang="ts">
 	import { base } from '$app/paths';
 	import DemoLink from '$lib/components/DemoLink.svelte';
+	import Logo from '$lib/components/Logo.svelte';
 </script>
 
-<section class="max-w-screen-lg m-auto px-6 box-content flex flex-col h-full gap-4">
-	<div class="py-8 md:h-80">
-		<h1 class="text-5xl md:text-6xl">
-			<span class="text-[#10B8B4]">Flottform</span> in action: check our demos
+<div
+	class="bg-secondary-blue text-white pt-4 pb-4 sm:pt-4 sm:pb-4 grid place-items-center text-lg font-normal"
+>
+	<div
+		class="flex flex-col sm:flex-row items-center gap-4 place-content-between w-full max-w-screen-lg mx-auto px-6"
+	>
+		<a href="{base}/"> <Logo /></a>
+		<nav>
+			<ul class="flex flex-col items-center sm:items-start sm:flex-row flex-wrap gap-2 sm:gap-4">
+				<li>
+					<a
+						href="https://flottform.io/use-cases"
+						target="_blank"
+						rel="external noopener noreferrer">Flottform Use Cases</a
+					>
+				</li>
+				<li>
+					<a href="https://flottform.io/contact" target="_blank" rel="external noopener noreferrer"
+						>Contact Us</a
+					>
+				</li>
+			</ul>
+		</nav>
+	</div>
+</div>
+<section class="max-w-screen-lg mx-auto px-6 box-content flex flex-col h-full gap-20 py-20">
+	<div class="">
+		<h1 class="text-5xl md:text-6xl text-black">
+			<span
+				class="bg-gradient-to-r from-secondary-blue to-secondary-green inline-block text-transparent bg-clip-text"
+				>Flottform</span
+			> in action: check our demos
 		</h1>
 	</div>
-	<div class="flex gap-6 flex-1 flex-wrap py-4">
+	<div class="grid grid-cols-1 gap-4 sm:gap-8 sm:grid-cols-2 md:grid-cols-3">
 		<DemoLink
 			href="{base}/default-ui"
 			title="Default UI"
@@ -23,6 +52,11 @@
 			description="See how Flottform can power your forms without any default UI constraints. This demo
 				showcases how you can leverage Flottform's functionality while styling your forms exactly as
 				you need, offering complete freedom to match your design preferences."
+		/>
+		<DemoLink
+			href="{base}/customized-default-ui"
+			title="Customized default UI"
+			description="See how you can tailor Flottform's default UI to better match your design, while still retaining all the powerful features of the original interface. This demo lets you explore how easy it is to adapt the default elements to fit seamlessly with your brand's style."
 		/>
 	</div>
 </section>
