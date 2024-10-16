2300bd6876bc1833b6848bfb88c8a438c5f14e82
Joern Bernhardt <joern.bernhardt@googlemail.com>
2024-10-17 00:44:15 +0200

    chore: Remove debug logs
    Signed-off-by: Joern Bernhardt <joern.bernhardt@googlemail.com>

--- flottform/server-standalone/src/lib/cors-headers.ts
+++ flottform/server-standalone/src/lib/cors-headers.ts
@@ -15,8 +15,6 @@ export const corsHeaders = (allowedMethods: Array<HttpMethod>, request: Request)
 	const allowedOrigins = env.ALLOWED_ORIGINS.split(',');
 	const originHeader = request.headers.get('Origin');
 	const allowedOrigin = allowedOrigins.find((x) => x === originHeader) ?? allowedOrigins?.[0];
-	console.log('headers:', ...request.headers);
-	console.log({ allowedOrigins, allowedOrigin });
 	return {
 		...(allowedOrigin
 			? { 'Access-Control-Allow-Origin': allowedOrigin }
