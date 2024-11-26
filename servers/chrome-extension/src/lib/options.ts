1d8ed746ae194c2d71b27afbd3fffce2770ba6e0
Joern Bernhardt <joern.bernhardt@googlemail.com>
2024-11-26 23:00:09 +0100

    chore: Use demo.flottform.io as default
    Signed-off-by: Joern Bernhardt <joern.bernhardt@googlemail.com>

--- servers/chrome-extension/src/lib/options.ts
+++ servers/chrome-extension/src/lib/options.ts
@@ -1,3 +1,3 @@
 export const defaultTokenValue = '';
-export const defaultSignalingServerUrlBase = 'https://192.168.0.21:5177/flottform';
-export const defaultExtensionClientUrlBase = 'https://192.168.0.21:5175/browser-extension';
+export const defaultSignalingServerUrlBase = 'https://demo.flottform.io/flottform';
+export const defaultExtensionClientUrlBase = 'https://demo.flottform.io/browser-extension';
