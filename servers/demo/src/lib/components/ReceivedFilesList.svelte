64334ca1b8f3a29ee7a01ccd1a966ef8beccfded
nidhal-labidi <nidhal.labidi@compose.us>
2025-01-29 10:35:29 +0100

    feat: add automatic scrolling to the bottom of the list of received files
    Signed-off-by: nidhal-labidi <nidhal.labidi@compose.us>

--- servers/demo/src/lib/components/ReceivedFilesList.svelte
+++ servers/demo/src/lib/components/ReceivedFilesList.svelte
@@ -1,23 +1,40 @@
 <script lang="ts">
-	export let receivedFiles;
-	export let removeFile: (index: number) => void;
+	let { receivedFiles, removeFile } = $props<{
+		receivedFiles: { url: string; name: string }[];
+		removeFile: (index: number) => void;
+	}>();
+
+	let filesContainer: HTMLDivElement | null = null;
+	function scrollToBottomOfMessages() {
+		if (filesContainer) {
+			filesContainer.scrollTop = filesContainer.scrollHeight;
+		}
+	}
+
+	$effect(() => {
+		if (receivedFiles.length > 0) {
+			scrollToBottomOfMessages();
+		}
+	});
 </script>
 
-{#each receivedFiles as file, index}
-	<li class="flex items-center justify-between p-3 bg-gray-100 rounded-lg">
-		<span class="text-sm">{file.name}</span>
-		<div class="flex flex-col">
-			<a href={file.url} download={file.name} class="text-green-500 hover:underline text-sm">
-				Download
-			</a>
-			<button
-				class="text-red-500 hover:underline text-sm"
-				onclick={() => {
-					removeFile(index);
-				}}
-			>
-				Delete
-			</button>
-		</div>
-	</li>
-{/each}
+<div class="h-[50vh] overflow-y-auto scroll-smooth" bind:this={filesContainer}>
+	{#each receivedFiles as file, index}
+		<li class="flex items-center justify-between p-3 m-2 bg-gray-100 rounded-lg">
+			<span class="text-sm">{file.name}</span>
+			<div class="flex flex-col">
+				<a href={file.url} download={file.name} class="text-green-500 hover:underline text-sm">
+					Download
+				</a>
+				<button
+					class="text-red-500 hover:underline text-sm"
+					onclick={() => {
+						removeFile(index);
+					}}
+				>
+					Delete
+				</button>
+			</div>
+		</li>
+	{/each}
+</div>
