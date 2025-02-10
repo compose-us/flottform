3bb995a3892b0e1786c96464efb60370d50aae3f
nidhal-labidi <nidhal.labidi@compose.us>
2025-02-10 10:59:47 +0100

    chore: open options page automatically upon install of the extension
    Signed-off-by: nidhal-labidi <nidhal.labidi@compose.us>

--- servers/chrome-extension/vite.config.ts
+++ servers/chrome-extension/vite.config.ts
@@ -1,9 +1,21 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
+import { fileURLToPath } from 'url';
+import { dirname, resolve } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 export default defineConfig({
 	plugins: [sveltekit()],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
+	},
+	build: {
+		rollupOptions: {
+			input: {
+				background: resolve(__dirname, 'src/background/background.js')
+			}
+		}
 	}
 });
