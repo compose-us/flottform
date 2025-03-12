d51833a6c0ac19246e21e7369f16af9916564b25
nidhal-labidi <nidhal.labidi@compose.us>
2025-03-12 15:14:53 +0100

    feat: use FlottformFileInput classes to show an example of bidirectional file exchange
    Signed-off-by: nidhal-labidi <nidhal.labidi@compose.us>

--- servers/demo/src/routes/+page.svelte
+++ servers/demo/src/routes/+page.svelte
@@ -64,5 +64,10 @@
 			title="Customized default UI"
 			description="See how you can tailor Flottform's default UI to better match your design, while still retaining all the powerful features of the original interface. This demo lets you explore how easy it is to adapt the default elements to fit seamlessly with your brand's style."
 		/>
+		<DemoLink
+			href="{base}/flottform-file-sharing"
+			title="Flottform File Sharing"
+			description="See how you can use Flottform to exchange files between two peers!"
+		/>
 	</div>
 </section>
